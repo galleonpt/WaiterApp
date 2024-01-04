910d356f67f291aac41e6e0c66781c1470054124
galleonpt <jose15.luis00@gmail.com>
2024-01-04 00:38:44 +0000

    types: improve order types

--- frontend/src/types/Order.ts
+++ frontend/src/types/Order.ts
@@ -1,14 +1,18 @@
+interface IOrderProduct {
+  name: string;
+  imagePath: string;
+  price: number;
+}
+
+interface IOrderProducts {
+  _id: string;
+  quantity: number;
+  product: IOrderProduct;
+}
+
 export interface IOrder {
   _id: string;
   table: string;
   status: 'WAITING' | 'IN_PRODUCTION' | 'DONE';
-  products: {
-    _id: string;
-    quantity: number;
-    product: {
-      name: string;
-      imagePath: string;
-      price: number;
-    };
-  }[];
+  products: IOrderProducts[];
 }
