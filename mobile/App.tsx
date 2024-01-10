bafec8b15742cdef37462fb1286cf31e227a2d48
galleonpt <jose15.luis00@gmail.com>
2024-01-10 13:16:52 +0000

    core: add global font family

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,22 @@
+import { StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Text } from './src/components/Text';
 
 export default function App() {
+    const [isFontsLoaded] = useFonts({
+        'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+        'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+        'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf')
+    });
+
+    if(!isFontsLoaded) {
+        return null;
+    }
+
     return (
         <View style={styles.container}>
+            <Text>Open up App.tasdadssx to start working on your app!</Text>
             <Text>Open up App.tsx to start working on your app!</Text>
             <StatusBar style="auto" />
         </View>
