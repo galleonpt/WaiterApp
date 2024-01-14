a029127a3565a8210381c5c2312f9b8fb2c7ab9a
galleonpt <jose15.luis00@gmail.com>
2024-01-14 18:11:17 +0000

    feat: finish categories list component

--- mobile/src/components/Categories/Categories.tsx
+++ mobile/src/components/Categories/Categories.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { categories } from '../../mocks/categories';
 import { Text } from '../Text';
 import { Category, Icon } from './styles';
 
 const Categories = () => {
+    const [selectedCategory, setSelectedCategory] = useState('');
+
+    const handleSelectCategory = (categoryId: string) => {
+        const category = selectedCategory === categoryId ? '' : categoryId;
+        setSelectedCategory(category);
+    };
+
     return (
         <FlatList
             horizontal
@@ -11,15 +19,19 @@ const Categories = () => {
             data={categories}
             keyExtractor={category => category._id}
             contentContainerStyle= {{ paddingRight: 24 }}
-            renderItem={({item: category})=>(
-                <Category key={category._id}>
-                    <Icon>
-                        <Text>{category.icon}</Text>
-                    </Icon>
+            renderItem={({item: category})=>{
+                const isSelected = selectedCategory === category._id;
 
-                    <Text size={14} weight="600">{category.name}</Text>
-                </Category>
-            )}
+                return (
+                    <Category key={category._id} onPress={() => handleSelectCategory(category._id)}>
+                        <Icon>
+                            <Text opacity={isSelected? 1 : .5} >{category.icon}</Text>
+                        </Icon>
+
+                        <Text size={14} weight="600" opacity={isSelected? 1: .5} >{category.name}</Text>
+                    </Category>
+                );
+            }}
         />
     );
 };
